27007f4ff46adc75782b8b2bf29307239ad404c4
John Robin Davis <johnrobindavis@Johns-MacBook-Pro.local>
2023-08-02 21:27:35 +0530

    Change appId :x

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'price-calculator',
-  webDir: 'dist',
+  appId: "com.ng.price-calculator",
+  appName: "price-calculator",
+  webDir: "dist",
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: "https",
+  },
 };
 
 export default config;
